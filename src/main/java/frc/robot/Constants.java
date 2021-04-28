9bbc0d9de2e7b66a782fa20d71a477ab43500d03
Circuitree Laptop New <dontknowouremail@gmail.com>
2021-04-28 16:34:10 -0400

    Completing the code and adding accurate drive and angle motor ports

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,10 +1,28 @@
 package frc.robot;
 
 public class Constants {
-    /**
-     * How many milliseconds it takes for a CAN command to time out.
-     */
-    public static final int CAN_TIMEOUT_MS = 20;
+    public static final int DRIVETRAIN_FRONT_LEFT_DRIVE_MOTOR = 4;
+    public static final int DRIVETRAIN_FRONT_RIGHT_DRIVE_MOTOR = 2;
+    public static final int DRIVETRAIN_BACK_LEFT_DRIVE_MOTOR = 6;
+    public static final int DRIVETRAIN_BACK_RIGHT_DRIVE_MOTOR = 8;
 
-    public static final double DEADBAND_RANGE = 0.05;
+    public static final int DRIVETRAIN_FRONT_LEFT_ANGLE_MOTOR = 3;
+    public static final int DRIVETRAIN_FRONT_RIGHT_ANGLE_MOTOR = 1;
+    public static final int DRIVETRAIN_BACK_LEFT_ANGLE_MOTOR = 5;
+    public static final int DRIVETRAIN_BACK_RIGHT_ANGLE_MOTOR = 7;
+
+    public static final int DRIVETRAIN_FRONT_LEFT_ENCODER_PORT = 9;
+    public static final int DRIVETRAIN_FRONT_RIGHT_ENCODER_PORT = 10;
+    public static final int DRIVETRAIN_BACK_LEFT_ENCODER_PORT = 11;
+    public static final int DRIVETRAIN_BACK_RIGHT_ENCODER_PORT = 12;
+
+    public static final int PIGEON_PORT = 51;
+
+    // In degrees
+    public static final double DRIVETRAIN_FRONT_LEFT_ENCODER_OFFSET = Math.toRadians(214.225);
+    public static final double DRIVETRAIN_FRONT_RIGHT_ENCODER_OFFSET = Math.toRadians(118.0);
+    public static final double DRIVETRAIN_BACK_LEFT_ENCODER_OFFSET = Math.toRadians(251.15);
+    public static final double DRIVETRAIN_BACK_RIGHT_ENCODER_OFFSET = Math.toRadians(261.0);
+
+    public static final int PRIMARY_CONTROLLER_PORT = 0;
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,48 +1,52 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
-import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.ExampleCommand;
-import frc.robot.subsystems.ExampleSubsystem;
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.*;
+import frc.robot.commands.*;
+import frc.robot.subsystems.*;
+import frc.robot.common.Rotation2;
+import frc.robot.common.Axis;
+import frc.robot.common.XboxController;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and button mappings) should be declared here.
- */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+    private final XboxController primaryController = new XboxController(Constants.PRIMARY_CONTROLLER_PORT);
 
-  private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+    private final DrivetrainSubsystem drivetrainSubsystem = new DrivetrainSubsystem();
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    // Configure the button bindings
-    configureButtonBindings();
-  }
+    public RobotContainer() {
+        primaryController.getLeftXAxis().setInverted(true);
+        primaryController.getRightXAxis().setInverted(true);
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {}
+        CommandScheduler.getInstance().setDefaultCommand(drivetrainSubsystem, new DriveCommand(drivetrainSubsystem, getDriveForwardAxis(), getDriveStrafeAxis(), getDriveRotationAxis()));
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-    return m_autoCommand;
-  }
+        configureButtonBindings();
+    }
+
+    private void configureButtonBindings() {
+        primaryController.getBackButton().whenPressed(
+                () -> drivetrainSubsystem.resetGyroAngle(Rotation2.ZERO)
+        );
+        primaryController.getStartButton().whenPressed(
+                drivetrainSubsystem::resetWheelAngles
+        );
+    }
+
+    private Axis getDriveForwardAxis() {
+        return primaryController.getLeftYAxis();
+    }
+
+    private Axis getDriveStrafeAxis() {
+        return primaryController.getLeftXAxis();
+    }
+
+    private Axis getDriveRotationAxis() {
+        return primaryController.getRightXAxis();
+    }
+
+    public DrivetrainSubsystem getDrivetrainSubsystem() {
+        return drivetrainSubsystem;
+    }
+
+    public XboxController getPrimaryController() {
+        return primaryController;
+    }
 }
